488edb51a4c660bffa18e9054f5ad3e15a8f30ca
Dominic Torruellas <dom@domtorr.com>
2019-04-27 13:42:22 -0700

    animate menu open & close

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,22 @@
 import './index.css';
+import $ from 'jquery';
 
-document
-  .querySelector('.hamburger')
-  .addEventListener('click', () =>
-    document.querySelector('.nav-links').classList.toggle('show-links')
-  );
+// document
+//   .querySelector('.hamburger')
+//   .addEventListener('click', () =>
+//     document.querySelector('.nav-links').classList.toggle('hidden')
+//   );
+
+$('.nav-links').hide();
+$('.hamburger').on('click', () => {
+  const links = $('.nav-links');
+  if (links.css('display') === 'none') {
+    links.removeClass('out');
+    links.slideDown(200);
+    links.addClass('active');
+  } else {
+    links.removeClass('active');
+    links.slideUp(200);
+    links.addClass('out');
+  }
+});
