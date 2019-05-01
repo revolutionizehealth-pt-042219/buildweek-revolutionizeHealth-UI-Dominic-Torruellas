67e9ef4008b06552f330b0704a1462c831b36c04
Dominic Torruellas <dom@domtorr.com>
2019-05-01 16:21:13 -0700

    animating menu slide down and up without jQuery

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,7 @@
 import './index.css';
-import $ from 'jquery';
 
-// document
-//   .querySelector('.hamburger')
-//   .addEventListener('click', () =>
-//     document.querySelector('.nav-links').classList.toggle('hidden')
-//   );
-
-$('.hamburger').on('click', () => {
-  const links = $('.nav-links');
-  if (links.css('display') === 'none') {
-    links.removeClass('out');
-    links.slideDown(200);
-    links.addClass('active');
-    links.css('display', 'flex');
-  } else {
-    links.removeClass('active');
-    links.slideUp(200);
-    links.addClass('out');
-  }
-});
+document
+  .querySelector('.hamburger')
+  .addEventListener('click', () =>
+    document.querySelector('.nav-links').classList.toggle('visible')
+  );
